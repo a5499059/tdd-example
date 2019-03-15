178a18c262a9d6753d3cef6cd1c3b7841903cd30
Kei Sasaki <kei.sasaki@FVFXKBVNHV27.local>
2019-03-16 00:39:02 +0900

    chapter1-end

---
+++ src/money/Dollar.java
@@ -0,0 +1,15 @@
+package money;
+
+public class Dollar {
+    public int amount;
+
+    Dollar(int amount){
+
+        this.amount = amount;
+
+    }
+
+    public void times(int multiplier)  {
+        amount *= multiplier;
+    }
+}
