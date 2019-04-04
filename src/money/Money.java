cc6e88cfcc617281376123f7c1028274d95ef44d
Kei Sasaki <kei.sasaki@FVFXKBVNHV27.local>
2019-04-04 21:49:41 +0900

    chapter-8 end

--- src/money/Money.java
+++ src/money/Money.java
@@ -1,11 +1,21 @@
 package money;
 
-class Money {
+abstract class Money {
     protected int amount;
+    abstract Money times(int multiplier);
+
     @Override
     public boolean equals(Object o) {
         Money money = (Money) o;
         return amount == money.amount
                 && getClass().equals(money.getClass());
+    }
+
+    static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount);
     }
 }
